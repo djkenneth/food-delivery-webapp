e4f88395e0694942e805d98e09ad609a3d3eac15
djkenneth <dj.kenneth.pineda@gmail.com>
2024-07-02 16:17:43 +0800

    Update name

--- servers/apps/users/src/email/email.module.ts
+++ servers/apps/users/src/email/email.module.ts
@@ -19,7 +19,7 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
           },
         },
         defaults: {
-          from: 'Becodemy',
+          from: 'Food Delivery',
         },
         template: {
           dir: join(__dirname, '../../../../servers/email-templates'),
